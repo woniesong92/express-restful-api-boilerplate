39e02697f7ece7ba591c6c6f245e2193c3f9d4cf
woniesong92 <hs459@cornell.edu>
2016-06-18 02:16:28 -0700

    Update db file for Heroku

--- server/db.js
+++ server/db.js
@@ -2,6 +2,8 @@ import mongoose from 'mongoose'
 import http from 'http'
 
 export default function(callback) {
-  mongoose.connect('mongodb://localhost/myapp')
+  // Set DATABASE_URL environment variable on production
+  const DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/myapp'
+  mongoose.connect(DATABASE_URL)
 	callback()
 }
